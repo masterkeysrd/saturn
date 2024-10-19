f25b39e159359cec88f2d9813706bdf0864d2c47
David Morales <morales.david1997@gmail.com>
2024-10-19 00:59:40 -0400

    chore(tools): add openapi codegen as tool

---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+// +build tools
+
+// To install everything from this file, run:
+// go generate -tags tools tools/tools.go
+package main
+
+iport (
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+)
