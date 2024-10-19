571d87039796307ca49d81aa71703a9ccdae8b7c
David Morales <morales.david1997@gmail.com>
2024-10-19 01:03:24 -0400

    chore(api/expense): add basic lamba logic

---
+++ cmd/functions/expense/create/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
