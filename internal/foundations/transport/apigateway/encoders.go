729847f6d45b03dc6002a576f8d42e6f94ab0bbb
David Morales <morales.david1997@gmail.com>
2024-10-25 00:51:43 -0400

    chore(transport): implement error encoder

---
+++ internal/foundations/transport/apigateway/encoders.go
@@ -0,0 +1,38 @@
+package apigateway
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/masterkeysrd/saturn/internal/foundations/errors"
+)
+
+type ErrorEncoder func(ctx context.Context, err error) (events.APIGatewayProxyResponse, error)
+
+func defaultErrorEncoder(ctx context.Context, err error) (events.APIGatewayProxyResponse, error) {
+	status := 500
+	switch {
+	case errors.Is(err, errors.Invalid):
+		status = 400
+	case errors.Is(err, errors.Permission):
+		status = 403
+	case errors.Is(err, errors.NotExist):
+		status = 404
+	case errors.Is(err, errors.Exist):
+		status = 409
+	case errors.Is(err, errors.IO):
+		status = 500
+	case errors.Is(err, errors.Storage):
+		status = 500
+	case errors.Is(err, errors.Internal):
+		status = 500
+	case errors.Is(err, errors.Other):
+		status = 500
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       fmt.Sprintf(`{"error": "%s"}`, err.Error()),
+	}, nil
+}
