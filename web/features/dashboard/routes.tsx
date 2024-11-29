2427280a1f771eca6c42bd171d34fa91e2cb560f
David Morales <morales.david1997@gmail.com>
2024-11-29 16:27:48 -0400

    feat(web/dashboard): add basic view

---
+++ web/features/dashboard/routes.tsx
@@ -0,0 +1,9 @@
+import { RouteObject } from "react-router";
+import Dashboard from "./Dashboard";
+
+const DashboardRoutes: RouteObject = {
+  path: "dashboard",
+  element: <Dashboard />,
+};
+
+export default DashboardRoutes;
--- web/features/routes.ts
+++ web/features/routes.ts
@@ -1,6 +1,7 @@
 import { RouteObject } from "react-router";
 import { AuthRoutes } from "./auth";
+import DashboardRoutes from "./dashboard/routes";
 
-const FeatureRoutes: RouteObject[] = [AuthRoutes];
+const FeatureRoutes: RouteObject[] = [AuthRoutes, DashboardRoutes];
 
 export default FeatureRoutes;
