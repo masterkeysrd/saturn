1e4107997ab42723ef6a3f11c97e0a08c9b8046c
David Morales <morales.david1997@gmail.com>
2025-01-24 23:40:53 -0400

    chore(web): set-up react query

--- web/main.tsx
+++ web/main.tsx
@@ -2,23 +2,31 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import router from "./router";
 import theme from "./theme";
+import { AuthProvider } from "./lib/auth/AuthContext";
 
 // Import Roboto font
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { AuthProvider } from "./lib/auth/AuthContext";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <CssBaseline enableColorScheme />
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <CssBaseline enableColorScheme />
+          <RouterProvider router={router} />
+        </AuthProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 );
