c29d8768ffe99a5ed30e836630e2a55194da7cb9
David Morales <morales.david1997@gmail.com>
2025-12-13 00:25:29 -0400

    refactor: move web to apps

---
+++ apps/web/features/finance/components/SpentBreakdownChart.tsx
@@ -0,0 +1,91 @@
+import { useMemo } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { PieChart, type PieChartProps } from "@mui/x-charts/PieChart";
+import PieCenterLabel from "@/components/PieCenterLabel";
+import { money } from "@/lib/money";
+import type { SpendingBudgetSummary, SpendingSummary } from "../Finance.model";
+import { useMediaQuery, useTheme } from "@mui/material";
+
+export interface SpentBreakdownChartProps {
+  summary: SpendingSummary;
+  budgets: SpendingBudgetSummary[];
+}
+
+type ValueFormatter = PieChartProps["series"][number]["valueFormatter"];
+
+const getSeries = (budgets: SpendingBudgetSummary[]) =>
+  budgets.map((budget) => ({
+    label: budget.budget_name,
+    value: budget.spent.cents / 100,
+  }));
+
+export default function SpentBreakdownChart({
+  summary,
+  budgets,
+}: SpentBreakdownChartProps) {
+  const data = useMemo(() => getSeries(budgets), [budgets]);
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down("md"));
+  console.log("isSmall", isSmall);
+
+  const valueFormatter: ValueFormatter = (_, { dataIndex }) => {
+    const budget = budgets[dataIndex];
+    if (!budget) {
+      return null;
+    }
+
+    return money.format(budget.spent);
+  };
+
+  if (budgets.length === 0) {
+    return (
+      <Card sx={{ height: "100%" }}>
+        <CardContent
+          sx={{
+            height: 300,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            No expense data available
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    <Card>
+      <CardContent sx={{ height: 300 }}>
+        <Typography variant="subtitle2">Current Month Expenses</Typography>
+        <PieChart
+          sx={(theme) => ({
+            marginTop: theme.spacing(1.5),
+            width: "100%",
+            minWidth: 200,
+            // height: { xs: 240, sm: 280, md: 300 }, // responsive heights
+          })}
+          series={[
+            {
+              data,
+              valueFormatter,
+              innerRadius: 80,
+              outerRadius: 100,
+              paddingAngle: 0,
+              highlightScope: { fade: "global", highlight: "item" },
+            },
+          ]}
+        >
+          <PieCenterLabel
+            primaryText={money.format(summary.spent ?? money.zero())}
+            secondaryText={`of ${money.format(summary.budgeted ?? money.zero())} budgeted`}
+          />
+        </PieChart>
+      </CardContent>
+    </Card>
+  );
+}
