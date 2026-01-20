0e1141dda439585d93741e670a69ebd090437a2f
David Morales <morales.david1997@gmail.com>
2026-01-19 23:09:19 -0400

    fix(finance): rename setting state field to status

--- internal/storage/pg/migrations/20251224015859_create_table_finance_settings.sql
+++ internal/storage/pg/migrations/20251224015859_create_table_finance_settings.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS finance.settings (
   space_id UUID PRIMARY KEY,
-  state VARCHAR(16) NOT NULL DEFAULT 'incomplete',
+  status VARCHAR(16) NOT NULL DEFAULT 'INCOMPLETE',
   base_currency VARCHAR(3) NOT NULL DEFAULT 'USD',
   create_time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   create_by UUID NOT NULL,
