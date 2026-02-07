ae7114bfe7dbbe1f01cb32bc30c72c5c5a10deff
David Morales <morales.david1997@gmail.com>
2026-02-07 16:23:09 -0400

    refactor: remove unused code

--- internal/transport/http/controllers/identity/contracts.go
+++
@@ -1,9 +0,0 @@
-package identityhttp
-
-// type IdentityApplication interface {
-// 	RegisterUser(context.Context, *application.RegisterUserInput) (*identity.User, error)
-// 	LoginUser(context.Context, *application.LoginUserInput) (*application.TokenPair, error)
-// 	EndAllUserSessions(context.Context) error
-// 	RefreshSessionToken(context.Context, string) (*application.TokenPair, error)
-// 	RevokeSession(context.Context, string) error
-// }
